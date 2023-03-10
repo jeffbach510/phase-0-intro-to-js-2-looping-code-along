3541a70d6b1c8e4f90d5c8c9caaa5d299a2b63b9
Jeffrey Bachman <jeffbach510@sbcglobal.net>
2023-03-10 10:48:13 -0800

    changes to index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const messages = [];
+const newArray = [];
+
+function writeCards(messages,event) {
+  for (let i = 0; i < messages.length; i++) {
+
+    newArray[i] = `Thank you, ${messages[i]}, for the wonderful ${event} gift!`;
+  
+  };
+
+  return newArray;
+};
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+function countDown(num) {
+  let counter = num;
+  while (counter >= 0) {
+    console.log(counter--);
+  }
+  };
+  countDown(3);
